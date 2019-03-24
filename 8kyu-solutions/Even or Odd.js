25b5da9f13a3d75775093561aa2c5992919b4626
mook <godtwm@gmail.com>
2019-03-24 19:39:44 +0900

    8kyu-solutions..

---
+++ 8kyu-solutions/Even or Odd.js
@@ -0,0 +1,9 @@
+/**
+ * Question.
+ * Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+ * 정수인 인수를 짝수인 경우 "Even" 또는 홀수인 경우 "Odd"로 반환하시오.
+ */
+
+function even_or_odd(number) {
+  return (number%2 ===0) ? "Even" : "Odd";
+}
---
+++ 8kyu-solutions/Multiply.js
@@ -0,0 +1,9 @@
+/**
+ * Question.
+ * The code does not execute properly. Try to figure out why.
+ * 코드가 제대로 실행되지 않습니다. 이유를 찾아보십시오.
+ */
+
+function multiply(a, b){
+  return a * b
+}
