93eedab5265f662ed6c90c9f9ef83318c5530b45
mook9288 <mook9288@gmail.com>
2020-08-21 11:07:26 +0900

    modify. 코드 수정

--- Programmers/수박수박수박수박수박수/index.js
+++ Programmers/수박수박수박수박수박수/index.js
@@ -1,9 +1,18 @@
 function solution(n) {
   var answer = '';
+  var num = parseInt((n / 2) + 1);
   if (n % 2 === 0) {
-    answer = Array((n / 2) + 1).join("수박");
+    answer = Array(num).join("수박");
   } else {
-    answer = Array(parseInt((n / 2) + 1)).join("수박") + "수";
+    answer = Array(num).join("수박") + "수";
+  }
+  return answer;
+}
+
+function solution(n) {
+  var answer = '';
+  for (var i = 0; i < n; i++) { 
+    (i % 2 === 0) ? answer += "수" : answer += "박";
   }
   return answer;
 }
